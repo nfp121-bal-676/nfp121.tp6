affaefdf5963c7aeafbc0dd11512a4a89490fecd
Rami TOFEILY <rami.tofeily@isae.edu.lb>
2020-07-08 07:33:43 +0300

    finish question 4

--- question3/AbstractTransaction.java
+++ question3/AbstractTransaction.java
@@ -25,5 +25,10 @@ public abstract class AbstractTransaction{
             throw e;
         }
     }
-    
+    final public void credit(int somme) {
+        beginTransaction();
+        this.somme = somme;
+        cotisant.credit(somme);
+        endTransaction();
+    }
 }
